bf1a07936b1d13829e93428759c0409ef1e9bdb4
Snehal <snehaltikole14@gmail.com>
2024-12-15 21:18:39 +0530

    add files

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from "../Components/Header";
+import Header from '../Components/Header';
 import './signup.css'
-import Footer from "../Components/Footer"
+import Footer from '../components/Footer';
 
 
 const SignUp = () => {
